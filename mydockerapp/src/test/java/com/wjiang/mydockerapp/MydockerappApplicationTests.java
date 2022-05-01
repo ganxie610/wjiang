23b9a4b71e527e8a18a5decf9658676926839ec3
ganxie610 <ganxie610@aliyun.com>
2022-05-01 22:51:53 +0800

    add myapp

---
+++ mydockerapp/src/test/java/com/wjiang/mydockerapp/MydockerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjiang.mydockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydockerappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
